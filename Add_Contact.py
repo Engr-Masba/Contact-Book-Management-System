3869bebcd7a4b68478a30ec384a1266fe378bb08
Engr-Masba <masba.cuet09@gmail.com>
2024-12-05 00:38:58 +0600

    V02

--- Add_Contact.py
+++ Add_Contact.py
@@ -1,35 +1,51 @@
 from Save_All_Contacts import save_all_contacts
 
 def add_contacts(all_contact):
-    name = input("\nName: ").strip()
-    phone_number = input("Phone Number: ").strip()
-    email = input("Email: ").strip()
-    blood_group=input("Blood Group: ").strip()
-    address = input("Address: ").strip()
-
-    if phone_number and (not phone_number.isdigit() or not (10 < len(phone_number) < 15)):
-        print("\nInvalid phone number. Please enter numeric values only.")
-        return all_contact
-
-    if "@" not in email or "." not in email:
-        print("\nInvalid email address. Please enter a valid email.")
-        return all_contact
-
-    for contact in all_contact:
-        if contact["Phone_Number"] == phone_number:
-            print("\nA contact with this phone number already exists.")
+    
+    try:
+        name = input("\nName: ").strip()
+        if not name:
+            print("\n\tName can't be empty")
             return all_contact
+        
+        phone_number = input("Phone Number: ").strip()
+        if phone_number and (not phone_number.isdigit()):
+            print("\nInvalid phone number. Please enter numeric values only.")
+            return all_contact
+        for contact in all_contact:
+            if contact["Phone_Number"] == phone_number:
+                print("\nA contact with this phone number already exists.")
+                return all_contact
+        
+        email = input("Email: ").strip()
+        if email and ("@" not in email or "." not in email):
+            print("\nInvalid email address. Please enter a valid email.")
+            return all_contact
+        
+        if not phone_number and not email:
+            print("\nYou must provide a phone number or an email to add a contact")
+            return all_contact
+        
+        valid_blood_groups = {"A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"}
+        blood_group=input("Blood Group: ").strip().upper()
+        if blood_group and blood_group not in valid_blood_groups:
+            print("\nInvalid blood group. Please enter a valid blood group.")
+            return all_contact
+        
+        address = input("Address: ").strip()
 
-    contact = {
-        "Name": name,
-        "Phone_Number": phone_number,
-        "Email": email,
-        "Blood_Group": blood_group,
-        "Address": address,
-    }
+        contact = {
+            "Name": name,
+            "Phone_Number": phone_number,
+            "Email": email,
+            "Blood_Group": blood_group,
+            "Address": address,
+        }
 
-    all_contact.append(contact)
-    save_all_contacts(all_contact)
+        all_contact.append(contact)
+        print("\n\tContact Added Successfully!")
+        save_all_contacts(all_contact)
 
-    print("\nContact Added Successfully!")
+    except Exception as e:
+        print("\nAn error occured while adding contact.")
 
--- Delete_Contact.py
+++ Delete_Contact.py
@@ -18,16 +18,33 @@ def delete_contacts(all_contacts):
             if len(contacts_to_delete) > 1:
                 print("\nMultiple contacts found with the search criteria:")
                 for idx, contact in enumerate(contacts_to_delete, 1):
-                    print(f"{idx}. Name: {contact['Name']} | Phone: {contact['Phone_Number']}")
-                delete_index = int(input("\nEnter the number of the contact you want to delete: ")) - 1
-                contact_to_delete = contacts_to_delete[delete_index]
+                    print(f"{idx}. Name: {contact['Name']} | Phone: {contact['Phone_Number']} | Email: {contact['Email']} | Address: {contact['Address']}")
+                
+                delete_quiry=input("\nPress 'Y' to delete all or press any key: ").strip().lower()
+                if delete_quiry=='y':
+                    for c in contacts_to_delete:
+                        all_contacts.remove(c)
+                    print("\n\tAll matched contacts deleted")
+                    save_all_contacts(all_contacts)
+                    return
+                       
+                else:
+                    try:
+                        delete_index = int(input("\nEnter the number of the contact you want to delete: ")) - 1
+                        if delete_index<0 or delete_index>=len(contacts_to_delete):
+                            print("Invalid selection. No contact deleted.")
+                            return
+                        contact_to_delete = contacts_to_delete[delete_index]
+                    except ValueError:
+                        print("\n\tInvalid input. Enter a valid number.")
+                        
             else:
                 contact_to_delete = contacts_to_delete[0]
 
             all_contacts.remove(contact_to_delete)
-            save_all_contacts(all_contacts)
             print("\n\tContact Deleted Successfully.")
-
+            save_all_contacts(all_contacts)
+            
         except ValueError:
             print("\n\tInvalid input. Please enter a valid number when selecting contacts.")
         except Exception as e:
